d1d9601171d87be84c0dc16fcf9aee551398646d
rusok <rusok.software@gmail.com>
2026-06-16 21:26:24 -0400

    Formulario Frontend

---
+++ MiAplicacion/Controllers/CuentaController.cs
@@ -0,0 +1,40 @@
+﻿using MiAplicacion.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiAplicacion.Controllers
+{
+    [Route("cuenta")]
+    public class CuentaController : Controller
+    {
+        [Route("index")]
+        public IActionResult Index()
+        {
+            var cuentaViewModel = new CuentaViewModel()
+            {
+                cuenta = new Cuenta()
+                {
+                    Id = 1,
+                    Disponible = true,
+                    Genero = "F"
+                },
+                Lenguajes = new List<Lenguaje>()
+                {
+                    new Lenguaje() { Id = "len01", Nombre = "C#", estaMarcado = true },
+                    new Lenguaje() { Id = "len02", Nombre = "Java", estaMarcado = false },
+                    new Lenguaje() { Id = "len03", Nombre = "Python", estaMarcado = true },
+                    new Lenguaje() { Id = "len04", Nombre = "COBOL", estaMarcado = false }
+                }
+            };
+            var listaCargos = new List<Cargo>() {
+                new Cargo { Id = "car01", Nombre = "Desarrollador" },
+                new Cargo { Id = "car02", Nombre = "Auxiliar de desarrollo" },
+                new Cargo { Id = "car03", Nombre = "Administrador de Base de datos" }
+            };
+            cuentaViewModel.Cargos = new Microsoft.AspNetCore.Mvc.Rendering.SelectList(listaCargos, "Id", "Nombre");
+            return View("Index", cuentaViewModel);
+        }
+
+        //public IActionResult Registrar() { 
+        //}
+    }
+}
---
+++ MiAplicacion/Models/Cuenta.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MiAplicacion.Models
+{
+    public class Cuenta
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Password { get; set; }
+        public string Descripcion { get; set; }
+        public bool Disponible { get; set; }
+        public string Genero { get; set; }
+        public List<string> Lenguajes { get; set; }
+        public string Cargo { get; set; }
+    }
+    public class Lenguaje { 
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public bool estaMarcado { get; set; }
+    }
+    public class Cargo {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+    }
+    public class CuentaViewModel
+    {
+        public Cuenta cuenta { get; set; }
+        public List<Lenguaje> Lenguajes { get; set; }
+        public SelectList Cargos { get; set; }
+    }
+}
