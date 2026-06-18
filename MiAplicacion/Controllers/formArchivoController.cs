b5eeccb399757b40572868a6919bb28a6763a7e0
rusok <rusok.software@gmail.com>
2026-06-17 21:22:34 -0400

    Formulario con archivo

---
+++ MiAplicacion/Controllers/formArchivoController.cs
@@ -0,0 +1,37 @@
+﻿using MiAplicacion.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiAplicacion.Controllers
+{
+    [Route("archivo")]
+    public class formArchivoController : Controller
+    {
+        private IWebHostEnvironment webHostEnvironment;
+
+        public formArchivoController(IWebHostEnvironment webHostEnvironment)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+        }
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View("Index", new Producto());
+        }
+        [Route("Guardar")]
+        [HttpPost]
+        public IActionResult Guardar(Producto prod, IFormFile foto) {
+            if (foto == null || foto.Length == 0)
+                return Content("Error: Foto no seleccionada");
+            else {
+                // "C:\Users\Pepe\Desktop" + "img.jpg"
+                // "/home/Pepe/Escritorio" + "img.jpg"
+                var ruta = Path.Combine(webHostEnvironment.WebRootPath, "imagenes", foto.FileName);
+                var flujo = new FileStream(ruta, FileMode.Create);
+                foto.CopyToAsync(flujo);
+                prod.Foto = foto.FileName;
+            }
+            ViewBag.producto = prod;
+            return View("ExitoArchivo"); 
+        }
+    }
+}
