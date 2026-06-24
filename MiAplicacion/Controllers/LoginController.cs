1626a9504e26b86e79e1c4e14d93e819e2d8d06c
rusok <rusok.software@gmail.com>
2026-06-23 20:46:04 -0400

    Login con sesiones

---
+++ MiAplicacion/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiAplicacion.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string usuario, string password) {
+            if (usuario != null && password != null && usuario.Equals("pepe") && password.Equals("patito123")){
+                HttpContext.Session.SetString("usu", usuario);
+                return View("exito");
+            }
+            else {
+                ViewBag.error = "¡Credenciales incorrectas!";
+                return View("Index");
+            }
+        }
+        [HttpGet]
+        public IActionResult Logout() {
+            HttpContext.Session.Remove("usu");
+            return RedirectToAction("Index");
+        }
+    }
+}
