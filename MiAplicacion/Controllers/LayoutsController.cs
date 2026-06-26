e6e67d263b1407322e5216491178295f4f3afb3d
rusok <rusok.software@gmail.com>
2026-06-25 20:58:34 -0400

    Layouts

---
+++ MiAplicacion/Controllers/LayoutsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiAplicacion.Controllers
+{
+    public class LayoutsController : Controller
+    {
+        public IActionResult Home()
+        {
+            ViewBag.titulo = "HOME";
+            return View();
+        }
+        public IActionResult Acerca()
+        {
+            return View();
+        }
+        public IActionResult Noticias()
+        {
+            return View();
+        }
+        public IActionResult Acerca1()
+        {
+            return View();
+        }
+        public IActionResult Acerca2()
+        {
+            return View();
+        }
+        public IActionResult Noticias1()
+        {
+            return View();
+        }
+        public IActionResult Noticias2()
+        {
+            return View();
+        }
+    }
+}
